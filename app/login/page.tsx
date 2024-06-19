efc1827c9660c9d364ca15a15364bba3c49906ca
NickFury <miaytac1@gmail.com>
2024-06-19 20:29:51 +0200

    supabase auth toegevoegd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,24 @@
-export default function Page() {
+import { login, signup } from './actions'
+
+export default function LoginPage() {
   return (
-    <div>login</div>
-  );
+    <form>
+      <div className="flex flex-col items-center gap-4">
+        <div className="w-80 flex justify-between">
+          <label htmlFor="email">Email:</label>
+          <input className="text-black" id="email" name="email" type="email" required />
+        </div>
+        <div className="w-80 flex justify-between">
+          <label htmlFor="password">Password:</label>
+          <input className="text-black" id="password" name="password" type="password" required />
+        </div>
+        <div className="w-80 flex justify-between">
+          <button className="w-full border rounded py-1" formAction={login}>Log in</button>
+        </div>
+        <div className="w-80 flex justify-between">
+          <button className="w-full border rounded py-1" formAction={signup}>Sign up</button>
+        </div>
+      </div>
+    </form>
+  )
 }

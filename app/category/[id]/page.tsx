eb74f1c4a90c0eeacfa6359371469336f0ea4919
NickFury <miaytac1@gmail.com>
2024-06-20 01:06:32 +0200

    category detail pagina met test data

--- app/category/[id]/page.tsx
+++ app/category/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import { SupabaseClient } from "@supabase/supabase-js";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 async function getCategory(supabase: SupabaseClient, id: number) {
@@ -16,6 +17,20 @@ async function getCategory(supabase: SupabaseClient, id: number) {
   return data[0];
 }
 
+async function getRecepts(supabase: SupabaseClient, catId: number) {
+  const { data, error } = await supabase
+    .from("recepts")
+    .select("*")
+    .eq("category_id", catId)
+    .order("name");
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+}
+
 export default async function Page({params}: {params:{id:number}}) {
   const supabase = createClient();
   const category = await getCategory(supabase, params.id);
@@ -24,8 +39,17 @@ export default async function Page({params}: {params:{id:number}}) {
     redirect('/by-category');
   }
 
+  const raw_recepts = await getRecepts(supabase, params.id);
+
+  // temp code: verwijder nadat er voldoende data is 
+  const recepts = new Array(20).fill(raw_recepts).flat()
+      .map((i, index) => ({...i, id: i.id + index}));
+
+
   return (<>
-    <div className="mb-2 bg-center bg-no-repeat" style={{
+    <a href="/by-category" className="block text-xl mb-4">&lt; back</a>
+
+    <div className="mb-16 bg-center bg-no-repeat" style={{
          backgroundImage: "url('/img/placeholder-" + (params.id%4||4) + ".jpg')"
     }}>
       <div className="flex items-center border p-16 text-4xl font-black
@@ -34,6 +58,17 @@ export default async function Page({params}: {params:{id:number}}) {
       </div>
     </div>
 
-
+    <div className="grid grid-cols-3 gap-4">
+      {recepts.map(row => (
+        <div key={row.id} className="flex items-center justify-center flex-col">
+          <Image 
+            src={'/img/placeholder-' + (row.id%4||4) + '.jpg'} 
+            alt={row.name}
+            width={200}
+            height={200}/>
+          {row.name} ({row.id})
+        </div>
+      ))}
+    </div>
   </>);
 }
